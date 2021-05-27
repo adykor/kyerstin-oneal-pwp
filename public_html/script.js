fbad25ee528f93ba7effff86f4927f4338c32710
Kyerstin O'Neal <kcrocker1@cnm.edu>
2021-05-27 17:34:24 -0600

    commented out additional libararies

--- public_html/script.js
+++ public_html/script.js
@@ -4,7 +4,7 @@ const header = document.querySelector("header");
 const shadow = document.querySelector(".shadow");
 const content = document.querySelector(".content");
 const section = document.querySelector("section");
-const image_container = document.querySelector(".imgContainer");
+// const image_container = document.querySelector(".imgContainer");
 const opacity = document.querySelectorAll(".opacity");
 const border = document.querySelector(".border");
 
@@ -28,7 +28,7 @@ window.addEventListener('scroll', () => {
     shadow.style.height = `${scroll * 0.5 + 300}px`;
 
     content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
-    image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
+    //image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
 
     border.style.width = `${scroll / (sectionY.top + section_height) *30}%`;
 })
